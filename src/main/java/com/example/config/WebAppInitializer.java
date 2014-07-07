ace917a5dbbbb7f5626c6b18381182aa376b431a
Miguel Guilherme <miguel.guilherme@inova-software.com>
2014-07-07 16:24:00 +0100

    - Added mavenLocal() to build.gradle - WebAppInitializer refactor

---
+++ src/main/java/com/example/config/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+		ctx.register(SpringWebConfig.class);  
+		ctx.setServletContext(servletContext);    
+		Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+		dynamic.addMapping("/");  
+		dynamic.setLoadOnStartup(1);  
+	}
+
+}
