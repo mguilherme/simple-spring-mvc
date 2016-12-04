30e1868c2ad4c8379abcb7e3ae68badb66f3882a
Miguel Guilherme <mguilherme82@gmail.com>
2016-12-04 14:38:44 +0000

    - Update gradle wrapper to version 3.2.1 - Refactor

--- src/main/java/com/example/config/WebAppInitializer.java
+++ src/main/java/com/example/config/WebAppInitializer.java
@@ -1,12 +1,12 @@
 package com.example.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
